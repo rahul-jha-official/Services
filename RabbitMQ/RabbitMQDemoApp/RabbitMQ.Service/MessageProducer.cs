0d2c3ed9ed1b08a8ce841bbf7ae9c2a5db940d23
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2025-06-20 09:40:56 +0530

    Add files via upload

---
+++ RabbitMQ/RabbitMQDemoApp/RabbitMQ.Service/MessageProducer.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Service.Interfaces;
+using RabbitMQ.Service.Models;
+using System.Text;
+
+namespace RabbitMQ.Service;
+
+public class MessageProducer(IOptions<RabbitMQConfiguration> options) : IMessageProducer, IDisposable
+{
+
+    private IConnection? _connection;
+    private IChannel? _channel;
+
+    public async Task BasicPublishAsync<T>(ExchangeConfiguration exchangeConfiguration, QueueConfiguration queueConfiguration, MessageConfiguration<T> messageConfiguration) where T : class
+    {
+        if (_connection == null || !_connection.IsOpen || _channel == null || !_channel.IsOpen)
+        {
+            await INIT();
+        }
+
+        if (_connection is null || _channel is null)
+        {
+            throw new InvalidOperationException("RabbitMQ connection or channel is not initialized.");
+        }
+
+
+        await _channel.ExchangeDeclareAsync(exchange: exchangeConfiguration.Name, type: ExchangeType.Fanout);
+
+        var serializedMessage = JsonConvert.SerializeObject(messageConfiguration.Message);
+        var body = Encoding.UTF8.GetBytes(serializedMessage);
+
+        await _channel.BasicPublishAsync(exchange: exchangeConfiguration.Name, 
+                                            routingKey: exchangeConfiguration.RouingKey, 
+                                            body: body,
+                                            mandatory: true,
+                                            basicProperties: messageConfiguration.Properties);
+    }
+
+    private async Task INIT()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = options.Value.HostName,
+            UserName = options.Value.UserName,
+            Password = options.Value.Password,
+            Port = options.Value.Port
+        };
+
+        
+        _connection = await factory.CreateConnectionAsync();
+        _channel = await _connection.CreateChannelAsync();
+    }
+
+    public void Dispose()
+    {
+        _channel?.Dispose();
+        _connection?.Dispose();
+    }
+}
---
+++ RabbitMQ/RabbitMQDemoApp/RabbitMQ.Service/MessageReceiver.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Service.Interfaces;
+using RabbitMQ.Service.Models;
+using System.Text;
+using System.Threading.Channels;
+
+namespace RabbitMQ.Service;
+
+public class MessageReceiver(IOptions<RabbitMQConfiguration> options) : IMessageReceiver, IDisposable
+{
+    private IConnection? _connection;
+    private IChannel? _channel;
+
+    public async Task ReceiveMessage<T>(ExchangeConfiguration exchangeConfiguration, QueueConfiguration queueConfiguration, Func<T, Task> messageHandler) where T : class
+    {
+        if (_connection == null || !_connection.IsOpen || _channel == null || !_channel.IsOpen)
+        {
+            await INIT();
+        }
+
+        if (_connection is null || _channel is null)
+        {
+            throw new InvalidOperationException("RabbitMQ connection or channel is not initialized.");
+        }
+
+        await _channel.ExchangeDeclareAsync(exchange: exchangeConfiguration.Name, type: ExchangeType.Fanout);
+
+        await _channel.QueueDeclareAsync(queue: queueConfiguration.QueueName,
+                                        durable: queueConfiguration.Durable,
+                                        exclusive: queueConfiguration.Exclusive,
+                                        autoDelete: queueConfiguration.AutoDelete,
+                                        arguments: queueConfiguration.Arguments);
+
+        await _channel.QueueBindAsync(queue: queueConfiguration.QueueName,
+                                    exchange: exchangeConfiguration.Name,
+                                    routingKey: exchangeConfiguration.RouingKey);
+
+        await _channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+
+        var consumer = new AsyncEventingBasicConsumer(_channel);
+
+        consumer.ReceivedAsync += async (message, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var serializedMessage = Encoding.UTF8.GetString(body);
+            var deserializedMessage = JsonConvert.DeserializeObject<T>(serializedMessage);
+            await messageHandler(deserializedMessage!);
+        };
+
+        await _channel.BasicConsumeAsync(queue: queueConfiguration.QueueName, autoAck: true, consumer: consumer);
+    }
+
+    private async Task INIT()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = options.Value.HostName,
+            UserName = options.Value.UserName,
+            Password = options.Value.Password,
+            Port = options.Value.Port
+        };
+
+        _connection = await factory.CreateConnectionAsync();
+        _channel = await _connection.CreateChannelAsync();
+    }
+
+    public void Dispose()
+    {
+        _channel?.Dispose();
+        _connection?.Dispose();
+    }
+}
